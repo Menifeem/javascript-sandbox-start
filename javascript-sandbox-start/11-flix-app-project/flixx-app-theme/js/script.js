61c43a1cb9aa41750ea79016138cb1fcee1195e4
Menifeem <marquez_menifee@hotmail.com>
2025-12-09 21:48:44 -0500

    Flix Project Completion

--- javascript-sandbox-start/11-flix-app-project/flixx-app-theme/js/script.js
+++ javascript-sandbox-start/11-flix-app-project/flixx-app-theme/js/script.js
@@ -1,6 +1,23 @@
 //  Create a function within other functions to fetch data rather than calling fetch from every function where we need it
 
 
+
+// Global object (can be accessed throughout any function)
+const global = {
+  currentPage: window.location.pathname,
+  search: {                                                                                     // search object which will have anything to do with our with our search state
+    term: '',
+    type: '',
+    page: 1,
+    totalPages: 1,
+    totalResults: 0
+  },
+  api: {
+    apiKey: '0ab3596a0ae9a57d68f84a72cdf0423c',
+    apiUrl: 'https://api.themoviedb.org/3/',
+  }
+}
+
 // Calling the fetch data below from this function
 async function displayPopularMovies() {
     const { results } = await fetchAPIData('movie/popular');                                     // So anytime we need to get data from the API, we'll call this function and we'll pass in the endpoint
@@ -39,7 +56,7 @@ async function displayPopularMovies() {
 
 
 
-// Display pPopular TV Shows
+// Display Popular TV Shows
 async function displayPopularShows() {
     const { results } = await fetchAPIData('tv/popular');                                     // So anytime we need to get data from the API, we'll call this function and we'll pass in the endpoint
     
@@ -235,6 +252,143 @@ function displayBackgroundImage(type, backgroundPath) {                         
 
 
 
+
+// Search Movies/Shows
+async function search() {
+
+  // 1st get the data from url
+  const queryString = window.location.search;
+  // data from search 
+  const urlParams = new URLSearchParams(queryString);
+
+
+  // set global
+  global.search.type = urlParams.get('type');                                       // it's going to be dot search dot type and I'm going to set that to. Whatever that type is
+  global.search.type = urlParams.get('search-term');                                // Doing the same but with the search term
+
+
+  // Test to confirm the term is there 
+  if (global.search.term !== '' && global.search.term !== null) {
+    // Making the request(Creating a function)
+    const { results, totalPages, page, totalResults } = await searchAPIData();                    // Grab results data , then total pages and then the actual page #
+
+    // adding to global state
+    global.search.page = page;
+    global.search.totalPages = total_pages;
+    global.search.totalResults = total_results;
+
+    // Making sure that there are results
+    if (results.length === 0) {
+      showAlert('No results found');
+      return;
+    }
+
+    // add results to DOM if there are returns
+    displaySearchResults(results);
+    
+    document.querySelector('search-term').value= '';                                  // clear the input
+    // todo - make request and display results
+  } else {
+    showAlert('Please enter a search term'); //'error'//);                               // when we call call it here, we'll pass in a second argument of a class of error(can be found under 'style.css')
+  }
+}
+
+
+// Display Search Results (Results is an array passed in so we need to loop through it)
+function displaySearchResults(results) {
+  //clear previous results
+  document.querySelector('#search-results').innerHTML = '';
+  document.querySelector('#search-results-heading').innerHTML = '';
+  document.querySelector('#pagination').innerHTML = '';
+
+
+
+  results.forEach(result => {
+        const div = document.createElement('div');                                              // document dot create element, and we're creating a div
+        div.classList.add('card');                                                              // div and we'll do class list, dot add and we're going to add the class of card
+
+                                                                                                // what we're saying here is if there's a movie poster path, because if there's no no image, this will be null or false or something. But if it is there, we're going to show that image. If not, then we're going to we're going to show that flicks
+        div.innerHTML = `<a href="${global.search.type}-details.html?id=${result.id}">                                     
+            ${
+                result.poster_path
+                ? `<img
+              src="https://image.tmdb.org/t/p/w500/${result.poster_path}"
+              class="card-img-top"
+              alt="${global.search.type === 'movie' ? result.title : result.name}"              // we need to make this a conditional and we can once again, we can check the type from the global state. So we'll say global dot search dot type if that is equal to movie. So if that is equal to movie, oops, we want to use a ternary. Then let's make let's do result dot title. Okay. Else then we want to do result.name
+            />` : `<img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="${global.search.type === 'movie' ? result.title : result.name}"
+            />`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${global.search.type === 'movie' ? result.title : result.name}</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${global.search.type === 'movie' ? result.release_date : result.first_air_date}</small>
+            </p>
+          </div>`;
+
+
+          document.querySelector('#search-results-heading').innerHTML = `<h2>${results.length} of ${global.search.totalResults} Results for ${global.search.term}</h2>`
+
+          //Adding to the Dom
+          document.querySelector('#search-results').appendChild(div);
+    });
+
+
+    displayPagination();
+}
+
+
+
+
+// Create and Display Pagination for Search
+function displayPagination() {
+  const div = document.createElement('div');                                // create element and then we want to create a div
+  div.classList.add('pagination');                                          // Creating a Class to the div > Add class of 'pagination'
+  div.innerHTML = `                                                         // setting the innerhtml
+  <button class="btn btn-primary" id="prev">Prev</button>
+  <button class="btn btn-primary" id="next">Next</button>
+  <div class="page-counter">${global.search.page} of ${global.search.totalPages}</div>
+  `;
+
+
+  document.querySelector('#pagination').appendChild(div);                   // let's say document dot, query selector, and we're going to select the ID of pagination and then we're going to append child and pass in div.
+
+
+
+  // Disable Prev button if on first page
+  if(global.search.page === 1) {
+    document.querySelector('#prev').disabled = true;
+  }
+
+
+  // Disable Next button if on last page
+  if(global.search.page === global.search.totalPages) {
+    document.querySelector('#pnext').disabled = true;
+  }
+
+
+  // Next page (add an event listener so that we can actually change the page.)
+  document.querySelector('#next').addEventListener('click', async () => {
+    global.search.page++;                                                   // to increment the the global search page (by 1)
+    const { results, total_pages } = await searchAPIData();                  // we want to make a request and we're going to get from that request the results, of course. And then I also want to get the total underscore pages >>> we'll set that to await the search API data function
+    displaySearchResults(results);                                          // So let's call display search results and pass in the results
+  });
+
+
+  // Prev page
+  document.querySelector('#prev').addEventListener('click', async () => {
+    global.search.page--;                                                   // to decrement the the global search page (by 1)
+    const { results, total_pages } = await searchAPIData();                  // we want to make a request and we're going to get from that request the results, of course. And then I also want to get the total underscore pages >>> we'll set that to await the search API data function
+    displaySearchResults(results);                                          // So let's call display search results and pass in the results
+  });
+}
+
+
+
+
 // Display Slider Movies
 async function displaySlider() {                                                      // Made function async sine we are fetching from an API
   const { results } = await fetchAPIData('movie/now_playing');                        // destructure the results and we want to await on the fetch API data function and the endpoint that we're going to want to hit is going to be movie slash now playing
@@ -296,8 +450,8 @@ function initSwiper() {
 
 // Fetch Data from TMDB API
 async function fetchAPIData(endpoint) {                                         // call function fetchAPIData' and take in an endpoint
-    const API_KEY = '0ab3596a0ae9a57d68f84a72cdf0423c';                         // variable for api key > Note: this is not the path to go when in production
-    const API_URL = 'https://api.themoviedb.org/3/';
+    const API_KEY = global.apiKey;                                              // variable for api key > Note: this is not the path to go when in production > calling from the global function above
+    const API_URL = global.apiUrl;
 
 
     showSpinner();                                                              // Show spinnner is called here before making a request
@@ -313,6 +467,24 @@ async function fetchAPIData(endpoint) {                                         
 }
 
 
+// Make request to search
+async function searchAPIData() {                                                // call function searchAPIData' and doesnt take in anything
+    const API_KEY = global.apiKey;                                              // variable for api key > Note: this is not the path to go when in production > calling from the global function above
+    const API_URL = global.apiUrl;
+
+
+    showSpinner();                                                              // Show spinnner is called here before making a request
+
+    const response = await fetch(`${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}&page=${global.search.page}`);            // Search because the API URL has the ending slash included, so we don't need to put it, but it's going to be slash search and then let's do Slash and we'll put in here our global dot search dot type because remember it has to be either slash movie or slash TV. So that is in our type and whatever we select the radio button for gets put in our global state. So now I'm just putting it here in the URL. Then after that we're going to need, let's see, we have our API key and let's see, I have the language
+
+    const data = await response.json();                                         // Here we are obtaining the data > we want to await on the promise we get from response dot JSON
+
+    hideSpinner();                                                              // hide spinner called once the data is actually fetched before we return it
+
+    return data;                                                                // returning that data
+
+}
+
 
 // Show Spinner
 function showSpinner() {
@@ -326,6 +498,17 @@ function hideSpinner() {
 }
 
 
+
+
+// Show Alert
+function showlert(message, className = 'error') {
+  const alertEl = document.createElement('div');                                  //  we want to create is a div
+  alertEl.classList.add('alert', className);                                      // have the class of alert, so I'll do that and then we can add another argument to add another class
+  alertEl.appendChild(document.createTextNode(message));                          // say alert L and we're going to append child and then we want to append the text node. So document dot, create text node, and that text is going to be the message that comes in from the function
+  document.querySelector('alert').appendChild(alertEl);                           // add to the DOM > with ID of 'alert' > and then we're going to append child onto that and then we're going to add the alert element
+  
+  setTimeout(() => alertEl.remove(), 3000);                                        // Removes the alert that displays after 3 seconds when nothing is entered
+}
 
 
 // Function for the commas within the budget/revenue amounts etc
@@ -354,7 +537,7 @@ function init() {
             displayShowDetails();
             break;
         case '/search.html':
-            console.log('Search');
+            search();
             break;
     }
 }
